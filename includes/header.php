be57e542b7af712c2d396a8913610a1d56043d72
ezekia-shabani <ezekielshabani45@gmail.com>
2024-06-06 16:28:19 +0300

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,54 @@
+
+<header>
+  <div class="default-header">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-3 col-md-2">
+          <div class="logo"> <a href="index.php"><img src="assets/images/logg.png" alt="image"/></a> </div>
+        </div>
+        <div class="col-sm-9 col-md-10">
+          <div class="header_info">
+            <div class="header_widgets">
+   <?php   if(strlen($_SESSION['login'])==0)
+	{
+?>
+ <div class="login_btn"> <a href="#loginform" class="btn btn-xs uppercase" data-toggle="modal" data-dismiss="modal">Login / Register</a> </div>
+<?php }
+else{
+
+echo "Welcome To Car rental portal";
+ } ?>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Navigation -->
+  <nav id="navigation_bar" class="navbar navbar-default">
+    <div class="container">
+      <div class="navbar-header">
+        <button id="menu_slide" data-target="#navigation" aria-expanded="false" data-toggle="collapse" class="navbar-toggle collapsed" type="button"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+      </div>
+      <div class="header_wrap">     
+        </div>
+        <div class="header_search">
+          
+        </div>
+      </div>
+      <div class="collapse navbar-collapse" id="navigation">
+        <ul class="nav navbar-nav">
+          <li><a href="index.php">Home</a>    </li>
+
+          <li><a href="page.php?type=aboutus">About Us</a></li>
+          <li><a href="car-listing.php">Car Listing</a>
+          
+          <li><a href="contact-us.php">Contact Us</a></li>
+
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- Navigation end -->
+
+</header>
